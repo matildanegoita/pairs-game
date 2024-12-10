609d591731287e5d273d328d24e785287d6dae39
matildanegoita <negoita.cl.ioana@student.utcluj.ro>
2024-12-10 13:08:20 +0200

    game card component

---
+++ src/app/game-card/game-card.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-card',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      game-card works!
+    </p>
+  `,
+  styles: ``
+})
+export class GameCardComponent {
+
+}
