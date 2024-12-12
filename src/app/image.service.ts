1711ea312f977aea8dc47b37e4fb6fe9674d182b
matildanegoita <negoita.cl.ioana@student.utcluj.ro>
2024-12-12 11:26:40 +0200

    updated api

--- src/app/image.service.ts
+++ src/app/image.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ImageService {
+
+  private apiUrl = 'https://api.thecatapi.com/v1/images/search';
+  private apiKey = 'live_CALt6YQhRndNANx7hfFcfUbvpzggHKWz7LPhHIyryyzTVZ5LwJQG96J1ridukfce';
+
   constructor(private http: HttpClient) {}
 
   getImages(count: number): Observable<string[]> {
-    const urls = Array.from({ length: count }, (_, i) =>
-      `https://picsum.photos/200?random=${i}`
+    return this.http.get<any[]>(`${this.apiUrl}?limit=${count}&size=thumb`, {
+      headers: {
+        'x-api-key': this.apiKey,
+      }
+    }).pipe(
+      // Extrage URL-urile imaginilor
+      map((response) => response.map((item) => item.url))
     );
-    return new Observable((observer) => {
-      observer.next(urls);
-      observer.complete();
-    });
   }
 }
